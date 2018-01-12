85f1e95a6333393d20e4bc2a25441a107dababd4
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-01-12 18:03:26 +0100

    Added lstm_cell.py (not yet tested)

---
+++ time_sequence_prediction/lstm_cell.py
@@ -0,0 +1,63 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # noqa
+import matplotlib.pyplot as plt
+
+class LstmCell(nn.Module):
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(LstmCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+
+        self.w_i_i = nn.Parameter(torch.Tensor(input_size, hidden_size))
+        self.w_h_i = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
+        self.w_i_f = nn.Parameter(torch.Tensor(input_size, hidden_size))
+        self.w_h_f = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
+        self.w_i_g = nn.Parameter(torch.Tensor(input_size, hidden_size))
+        self.w_h_g = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
+        self.w_i_o = nn.Parameter(torch.Tensor(input_size, hidden_size))
+        self.w_h_o = nn.Parameter(torch.Tensor(hidden_size, hidden_size))
+
+        if bias:
+            self.b_i_i = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_h_i = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_i_f = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_h_f = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_i_g = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_h_g = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_i_o = nn.Parameter(torch.Tensor(hidden_size))
+            self.b_h_o = nn.Parameter(torch.Tensor(hidden_size))
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, hidden):
+        h_t, c_t = hidden
+
+        i = input.matmul(self.w_i_i) + h_t.matmul(self.w_h_i)
+        f = input.matmul(self.w_i_f) + h_t.matmul(self.w_h_f)
+        g = input.matmul(self.w_i_g) + h_t.matmul(self.w_h_g)
+        o = input.matmul(self.w_i_o) + h_t.matmul(self.w_h_o)
+
+        if self.bias:
+            i += self.b_i_i + self.b_h_i
+            f += self.b_i_f + self.b_h_f
+            g += self.b_i_g + self.b_h_g
+            o += self.b_i_o + self.b_h_o
+
+        i = torch.sigmoid(i)
+        f = torch.sigmoid(f)
+        g = torch.tanh(g)
+        o = torch.sigmoid(o)
+        c_t = f * c_t + i * g
+        h_t = o * torch.tanh(c_t)
+
+        return h_t, c_t
