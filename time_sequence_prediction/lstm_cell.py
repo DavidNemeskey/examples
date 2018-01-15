5a8bd22f4d5403162f8563324515c61d99e5365b
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-01-15 13:00:21 +0100

    Added LstmCell2.

--- time_sequence_prediction/lstm_cell.py
+++ time_sequence_prediction/lstm_cell.py
@@ -4,6 +4,7 @@ import math
 import torch
 import torch.nn as nn
 
+
 class LstmCell(nn.Module):
     def __init__(self, input_size, hidden_size, bias=True):
         super(LstmCell, self).__init__()
@@ -51,6 +52,46 @@ class LstmCell(nn.Module):
             g += self.b_i_g + self.b_h_g
             o += self.b_i_o + self.b_h_o
 
+        i = torch.sigmoid(i)
+        f = torch.sigmoid(f)
+        g = torch.tanh(g)
+        o = torch.sigmoid(o)
+        c_t = f * c_t + i * g
+        h_t = o * torch.tanh(c_t)
+
+        return h_t, c_t
+
+
+class LstmCell2(nn.Module):
+    def __init__(self, input_size, hidden_size, bias=True):
+        super(LstmCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.bias = bias
+
+        self.w_i = nn.Parameter(torch.Tensor(input_size, 4 * hidden_size))
+        self.w_h = nn.Parameter(torch.Tensor(hidden_size, 4 * hidden_size))
+
+        if bias:
+            self.b_i = nn.Parameter(torch.Tensor(4 * hidden_size))
+            self.b_h = nn.Parameter(torch.Tensor(4 * hidden_size))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1.0 / math.sqrt(self.hidden_size)
+        for weight in self.parameters():
+            weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, hidden):
+        h_t, c_t = hidden
+
+        ifgo = input.matmul(self.w_i) + h_t.matmul(self.w_h)
+
+        if self.bias:
+            ifgo += self.b_i + self.b_h
+
+        i, f, g, o = ifgo.chunk(4, 1)
         i = torch.sigmoid(i)
         f = torch.sigmoid(f)
         g = torch.tanh(g)
