5b5b373ba8af7e6eb0e58822fc957d1d9b393893
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2018-01-16 17:13:33 +0100

    Added the tensorflow implementations.

---
+++ word_language_model/main_tf.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8 :
+
+"""Generic language modeling with RNN."""
+
+import argparse
+import json
+import math
+import os
+import time
+
+import numpy as np
+# To get rid of stupid tensorflow logging. Works from version 0.12.1+.
+# See https://github.com/tensorflow/tensorflow/issues/1258
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'  # noqa
+import tensorflow as tf
+
+import data
+from tf_model import LSTMModel
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description='Modification of the PyTorch Wikitext-2 RNN/LSTM Language '
+                    'Model, so that it actually does what Zaremba (2014) '
+                    'described.')
+    parser.add_argument('--data', '-d', type=str, default='./data/wikitext-2',
+                        help='location of the data corpus')
+    parser.add_argument('--configuration', '-c', type=str, default='config.json',
+                        help='the configuration file (json).')
+    parser.add_argument('--model', type=str, default='LSTM',
+                        help='the model key name.')
+    parser.add_argument('--seed', type=int, default=1111, help='random seed')
+    parser.add_argument('--cuda', action='store_true', help='use CUDA')
+    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                        help='report interval')
+    parser.add_argument('--save', type=str,  default='model.pt',
+                        help='path to save the final model')
+    args = parser.parse_args()
+
+    try:
+        config = read_config(args.configuration, args.model)
+        return args, config
+    except Exception as e:
+        parser.error('Error reading configuration file: {}'.format(e))
+
+
+class AttrDict(dict):
+    """Makes our life easier."""
+    def __getattr__(self, key):
+        if key not in self:
+            raise AttributeError('key {} missing'.format(key))
+        return self[key]
+
+    def __setattr__(self, key, value):
+        if key not in self:
+            raise AttributeError('key {} missing'.format(key))
+        self[key] = value
+
+
+def read_config(config_file, model):
+    with open(config_file) as inf:
+        return AttrDict(json.load(inf)[model])
+
+
+def batchify(data, bsz, cuda):
+    """
+    Starting from sequential data, batchify arranges the dataset into columns.
+    For instance, with the alphabet as the sequence and batch size 4, we'd get
+    ┌ a g m s ┐
+    │ b h n t │
+    │ c i o u │
+    │ d j p v │
+    │ e k q w │
+    └ f l r x ┘.
+    These columns are treated as independent by the model, which means that the
+    dependence of e. g. 'g' on 'f' can not be learned, but allows more efficient
+    batch processing.
+    """
+    # Work out how cleanly we can divide the dataset into bsz parts.
+    nbatch = data.size(0) // bsz
+    # Trim off any extra elements that wouldn't cleanly fit (remainders).
+    data = data.narrow(0, 0, nbatch * bsz)
+    # Evenly divide the data across the bsz batches.
+    data = data.view(bsz, -1).t().contiguous()
+    if cuda:
+        data = data.cuda()
+    return data
+
+
+def get_batch(source, i, bptt, evaluation=False):
+    """
+    get_batch subdivides the source data into chunks of length bptt.
+    If source is equal to the example output of the batchify function, with
+    a bptt-limit of 2, we'd get the following two Variables for i = 0:
+    ┌ a g m s ┐ ┌ b h n t ┐
+    └ b h n t ┘ └ c i o u ┘
+    Note that despite the name of the function, the subdivison of data is not
+    done along the batch dimension (i.e. dimension 1), since that was handled
+    by the batchify function. The chunks are along dimension 0, corresponding
+    to the seq_len dimension in the LSTM.
+    """
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len].numpy()
+    target = source[i+1:i+1+seq_len].numpy()
+    return data, target
+
+
+# def run_epoch(session, model, data, epoch_size=0, verbose=0,
+#               global_step=0, writer=None):
+#     """
+#     Runs an epoch on the network.
+#     - epoch_size: if 0, it is taken from data
+#     - data: a DataLoader instance
+#     """
+#     # TODO: these two should work together better, i.e. keep the previous
+#     #       iteration state intact if epoch_size != 0; also loop around
+#     epoch_size = data.epoch_size if epoch_size <= 0 else epoch_size
+#     data_iter = iter(data)
+#     start_time = time.time()
+#     costs = 0.0
+#     iters = 0
+#     state = session.run(model.initial_state)
+#
+#     fetches = [model.cost, model.final_state, model.train_op]
+#     fetches_summary = fetches + [model.summaries]
+#     if verbose:
+#         log_every = epoch_size // verbose
+#
+#     for step in range(epoch_size):
+#         x, y = next(data_iter)
+#
+#         # feed_dict = {model.sequence: batch}
+#         # for i, (c, h) in enumerate(model.initial_state):
+#         #     feed_dict[c] = state[i].c  # CEC for layer i
+#         #     feed_dict[h] = state[i].h  # hidden for layer i
+#         feed_dict = {
+#             model.input_data: x,
+#             model.targets: y,
+#             model.initial_state: state
+#         }
+#
+#         if verbose and step % log_every == log_every - 1:
+#             cost, state, _, summary = session.run(fetches_summary, feed_dict)
+#             if writer:
+#                 writer.add_summary(summary, global_step=global_step)
+#             if model.is_training:
+#                 global_step += 1
+#         else:
+#             cost, state, _ = session.run(fetches, feed_dict)
+#         # logger.debug('Cost: {}'.format(cost))
+#         costs += cost
+#         iters += model.params.num_steps
+#         if verbose and step % log_every == log_every - 1:
+#             logger.debug(
+#                 "%.3f perplexity: %.3f speed: %.0f wps" %
+#                 (step * 1.0 / epoch_size, np.exp(costs / iters),
+#                  iters * model.params.batch_size / (time.time() - start_time))
+#             )
+#
+#     # global_step is what the user sees, i.e. if the output is verbose, it is
+#     # increased, otherwise it isn't
+#     if not verbose and model.is_training:
+#         global_step += 1
+#
+#     return np.exp(costs / iters), global_step
+
+
+def train(sess, model, corpus, train_data, criterion, epoch, lr, config, log_interval):
+    def to_str(f):
+        return corpus.dictionary.idx2word[f]
+
+    # Turn on training mode which enables dropout.
+    model.assign_lr(sess, lr)
+    total_loss = 0
+    start_time = time.time()
+    hidden = model.init_hidden(config.batch_size)
+    fetches = [model.cost, model.predictions, model.final_state, model.train_op]
+
+    for batch, i in enumerate(range(0, train_data.size(0) - 1, config.bptt)):
+        data, targets = get_batch(train_data, i, config.bptt)
+        print('DATA\n', np.vectorize(to_str)(data.data.cpu().numpy()))
+        print('TARGETS\n', np.vectorize(to_str)(targets.data.cpu().numpy()))
+
+        feed_dict = {
+            model.input_data: data,
+            model.targets: targets,
+            model.initial_state: hidden
+        }
+        cost, output, hidden, _ = sess.run(fetches, feed_dict)
+
+        indices = np.argmax(output, 2)
+        print('OUTPUT\n', np.vectorize(to_str)(indices.data.cpu().numpy()))
+
+        total_loss += cost
+
+        if batch % log_interval == 0 and batch > 0:
+            cur_loss = total_loss[0] / log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | '
+                  'ms/batch {:5.2f} | loss {:5.2f} | ppl {:8.2f}'.format(
+                      epoch, batch, len(train_data) // config.bptt, lr,
+                      elapsed * 1000 / log_interval, cur_loss, math.exp(cur_loss)))
+            total_loss = 0
+            start_time = time.time()
+
+
+def evaluate(model, corpus, data_source, criterion, batch_size, bptt):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0
+    ntokens = len(corpus.dictionary)
+    hidden = model.init_hidden(batch_size)
+    for i in range(0, data_source.size(0) - 1, bptt):
+        data, targets = get_batch(data_source, i, bptt, evaluation=True)
+        output, hidden = model(data, hidden)
+        output_flat = output.view(-1, ntokens)
+        targets_flat = targets.view(-1)
+        total_loss += len(data) * criterion(output_flat, targets_flat).data
+    return total_loss[0] / len(data_source)
+
+
+def main():
+    args, config = parse_arguments()
+
+    ###############################################################################
+    # Load data
+    ###############################################################################
+
+    corpus = data.Corpus(args.data)
+
+    train_batch_size = config.batch_size
+    eval_batch_size = 10
+    train_data = batchify(corpus.train, train_batch_size, args.cuda)
+    val_data = batchify(corpus.valid, eval_batch_size, args.cuda)
+    test_data = batchify(corpus.test, eval_batch_size, args.cuda)
+    eval_batch_size = 10
+
+    # Create the models and the global ops
+    with tf.Graph().as_default() as graph:
+        tf.set_random_seed(args.seed)
+        # init_scale = 1 / math.sqrt(args.num_nodes)
+        initializer = tf.random_uniform_initializer(-0.1, 0.1)
+
+        with tf.name_scope('Train'):
+            with tf.variable_scope("Model", reuse=False, initializer=initializer):
+                mtrain = LSTMModel(True, config.ntoken, config.nhid, config.nlayers,
+                                   config.batch_size, config.bptt, config.clip)
+        with tf.name_scope('Valid'):
+            with tf.variable_scope("Model", reuse=True, initializer=initializer):
+                mvalid = LSTMModel(False, config.ntoken, config.nhid, config.nlayers,
+                                   eval_batch_size, config.bptt, config.clip)
+        with tf.name_scope('Test'):
+            with tf.variable_scope("Model", reuse=True, initializer=initializer):
+                mtest = LSTMModel(False, config.ntoken, config.nhid, config.nlayers,
+                                  eval_batch_size, config.bptt, config.clip)
+        with tf.name_scope('Global_ops'):
+            init = tf.global_variables_initializer()
+
+    with tf.Session(graph=graph) as sess:
+        sess.run(init)
+
+    ###############################################################################
+    # Training code
+    ###############################################################################
+
+        # Loop over epochs.
+        lr = config.lr
+
+        # At any point you can hit Ctrl + C to break out of training early.
+        try:
+            for epoch in range(1, config.epochs + 1):
+                lr_decay = config.lr_decay ** max(epoch - config.decay_delay, 0.0)
+                lr = config.lr * lr_decay
+                epoch_start_time = time.time()
+                train(mtrain, corpus, train_data,
+                      epoch, lr, config, args.log_interval)
+                val_loss = evaluate(mvalid, corpus, val_data,
+                                    eval_batch_size, config.bptt)
+                print('-' * 89)
+                print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                      'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                                 val_loss, math.exp(val_loss)))
+                print('-' * 89)
+        except KeyboardInterrupt:
+            print('-' * 89)
+            print('Exiting from training early')
+
+        # Run on test data.
+        test_loss = evaluate(mtest, corpus, test_data,
+                             eval_batch_size, config.bptt)
+        print('=' * 89)
+        print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+            test_loss, math.exp(test_loss)))
+        print('=' * 89)
+
+
+if __name__ == '__main__':
+    main()
---
+++ word_language_model/tf_model.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+import tf.contrib.rnn as rnn
+
+class LSTMModel(object):
+    """Generic LSTM language model based on the PTB model in tf/models."""
+    def __init__(self, is_training, ntoken, nhid, nlayers, batch_size,
+                 bptt, clip, dropout=0.5, tie_weights=False):
+        self.is_training = is_training
+
+        dims = [batch_size, bptt]
+        self._input_data = tf.placeholder(
+            tf.int32, dims, name='input_placeholder')
+        self._targets = tf.placeholder(
+            tf.int32, dims, name='target_placeholder')
+
+        cell = rnn.MultiRNNCell(
+            [rnn.BasicLSTMCell(nhid, forget_bias=1.0, state_is_tuple=True)
+             for _ in range(nlayers)]
+        )
+        self._initial_state = cell.zero_state(batch_size, dtype=tf.float32)
+
+        with tf.device("/cpu:0"):
+            embedding = tf.get_variable(
+                'embedding', [ntoken, nhid], trainable=True, dtype=tf.float32)
+            inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+
+        outputs, state = tf.nn.dynamic_rnn(
+            inputs=inputs, cell=cell, dtype=tf.float32,
+            initial_state=self._initial_state)
+        self._final_state = state
+
+        softmax_w = tf.get_variable(
+            "softmax_w", [nhid, ntoken], dtype=tf.float32)
+        softmax_b = tf.get_variable(
+            "softmax_b", [ntoken], dtype=tf.float32)
+        logits = tf.einsum('ijk,kl->ijl', outputs, softmax_w) + softmax_b
+
+        cost = tf.contrib.seq2seq.sequence_loss(
+            logits,
+            self._targets,
+            tf.ones([batch_size, bptt], dtype=tf.float32),
+            average_across_timesteps=False,  # BPTT
+            average_across_batch=True)
+        self._cost = tf.reduce_sum(cost)
+        self._prediction = tf.reshape(
+            tf.nn.softmax(logits), [-1, bptt, ntoken])
+
+        if is_training:
+            self._lr = tf.Variable(0.0, trainable=False)
+            tvars = tf.trainable_variables()
+            grads = tf.gradients(self.cost, tvars)
+            if clip:
+                grads, _ = tf.clip_by_global_norm(grads, clip)
+            optimizer = tf.train.GradientDescentOptimizer(self._lr)
+            self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+            self._new_lr = tf.placeholder(
+                tf.float32, shape=[], name="new_learning_rate")
+            self._lr_update = tf.assign(self._lr, self._new_lr)
+        else:
+            self._train_op = tf.no_op()
+
+    def assign_lr(self, session, lr_value):
+        session.run(self._lr_update, feed_dict={self._new_lr: lr_value})
+
+    @property
+    def input_data(self):
+        return self._input_data
+
+    @property
+    def targets(self):
+        return self._targets
+
+    @property
+    def predictions(self):
+        return self._prediction
+
+    @property
+    def cost(self):
+        return self._cost
+
+    @property
+    def final_state(self):
+        return self._final_state
+
+    @property
+    def lr(self):
+        return self._lr
+
+    @property
+    def train_op(self):
+        return self._train_op
