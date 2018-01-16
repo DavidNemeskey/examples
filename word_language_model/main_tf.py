67f1eeb13f737937ed89f51dd304908d9770e5d3
David Nemeskey <nemeskeyd@gmail.com>
2018-01-16 18:03:45 +0100

    It works up to evaluate() (but I think the cost is too high)

--- word_language_model/main_tf.py
+++ word_language_model/main_tf.py
@@ -176,6 +176,8 @@ def train(sess, model, corpus, train_data, epoch, lr, config, log_interval):
 
     for batch, i in enumerate(range(0, train_data.size(0) - 1, config.bptt)):
         data, targets = get_batch(train_data, i, config.bptt)
+        if len(data) < config.bptt:
+            break
         print('DATA\n', np.vectorize(to_str)(data))
         print('TARGETS\n', np.vectorize(to_str)(targets))
 
@@ -185,14 +187,15 @@ def train(sess, model, corpus, train_data, epoch, lr, config, log_interval):
             model.initial_state: hidden
         }
         cost, output, hidden, _ = sess.run(fetches, feed_dict)
+        print('COST', cost)
 
         indices = np.argmax(output, 2)
-        print('OUTPUT\n', np.vectorize(to_str)(indices.data.cpu().numpy()))
+        print('OUTPUT\n', np.vectorize(to_str)(indices.data))
 
         total_loss += cost
 
         if batch % log_interval == 0 and batch > 0:
-            cur_loss = total_loss[0] / log_interval
+            cur_loss = total_loss / log_interval
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.2f} | '
                   'ms/batch {:5.2f} | loss {:5.2f} | ppl {:8.2f}'.format(
